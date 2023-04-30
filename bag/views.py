b0d4d4655f0b2bafcf708730363749b549e9bf83
Enquil <jim.e.olesen@gmail.com>
2023-04-30 20:01:08 +0000

    added subtotal calculator through bag_tools.py

--- bag/views.py
+++ bag/views.py
@@ -1,29 +1,23 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
-    """
-    Renders content of the shoppingbag
-    """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag.html')
 
 
 def add_to_bag(request, item_id):
-    '''
-    Adds specified quantity of product to bag
-    '''
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
-
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-    # Gets the users bag as a dict
     bag = request.session.get('bag', {})
 
-    if size:  # block that executes if size is an option
+    if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
@@ -31,7 +25,7 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] = quantity
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-    else:  # executes if size is not an option for the item that is being added
+    else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
         else:
@@ -42,16 +36,12 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    '''
-    View for handling removing and adding items from the bag itself
-    '''
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
-
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-    # Gets the users bag as a dict
     bag = request.session.get('bag', {})
 
     if size:
@@ -59,11 +49,36 @@ def adjust_bag(request, item_id):
             bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
-    else:  # executes if size is not an option for the item that is being added
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
         if quantity > 0:
             bag[item_id] = quantity
         else:
-            bag.pop[item_id]
+            bag.pop(item_id)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
