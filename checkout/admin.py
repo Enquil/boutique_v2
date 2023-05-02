71e30a7108e1e0e92119d35191a1158dfbc0b118
Enquil <jim.e.olesen@gmail.com>
2023-05-02 21:42:20 +0000

    create admin model for order

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_numer',
+                       'date',
+                       'delivery_cost',
+                       'order_total',
+                       'grand_total',)
+
+    fields = ('order_number',
+              'date',
+              'full_name',
+              'email',
+              'phone_number',
+              'country',
+              'post_code',
+              'town_or_city',
+              'street_adress1',
+              'street_adress2',
+              'county',
+              'delivery_cost',
+              'order_total',
+              'grand_total',)
+
+    list_display = ('order_number',
+                    'date',
+                    'full_name',
+                    'order_total',
+                    'delivery_cost',
+                    'grand_total',)
+    
+    ordering = ('-date',)
