f37b837025da24dfcc384836a984525ca1b52040
Enquil <jim.e.olesen@gmail.com>
2023-05-06 17:44:41 +0000

    connected basic stripe features

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N4luMJYoNg0IOWPUZjQeo4a2AfFfOuxtyVj3Z5A8zzzB1qVQt1QBNtiBbm00W1OOTvpwoHZ45CUQHPOt0RRqJIU00nghLd9xA',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
